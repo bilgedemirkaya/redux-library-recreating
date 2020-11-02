c3c9c0d96f3a6e1f4b72d7e7684aeac3f624f07a
bilgedemirkaya <59152610+bilgedemirkaya@users.noreply.github.com>
2020-11-02 12:08:45 +0300

    actions-reducers

--- index.js
+++ index.js
@@ -1,42 +1,4 @@
-
-// updating the state 
-function todos(state = [], action){
-    switch(action.type){
-        case 'ADD_TODO':
-            // the reason we didn't use push is, bcs this function should be a pure funtion. 
-            //Not modifying the existing state but returning a new one.
-            return state.concat([action.todo])
-        case 'REMOVE_TODO':
-            // it will loop over each item in the state array, and filter one out
-            return state.filter((todo) => todo.id !== action.id)
-        case 'TOGGLE_TODO':
-            // instead of hard coding each property we want to toggle, 
-            //object.assign which allows us to merge different objects together
-            return state.map((todo) = todo.id !== action.id ? todo : 
-            Object.assign({},todo,{complete: !todo.complete}))
-            // says create me brand new object, get and merge todo object, but change the complete property
-        default:
-            return state;
-            }  
-    }
-function goals(state = [], action){
-    switch(action.type){
-        case 'ADD_GOAL':
-            return state.concat([action.goal])
-        case 'REMOVE_GOAL':
-            return state.filter((goal) => goal.id !== action.id)
-        default: 
-            return state;
-    }
-}
-
-function app(state={},action){
-    return{
-        todos: todos(state.todos,action),
-        goals: goals(state.goals,action)
-    }
-}
-
+// library code
 function createStore(reducer){
     // store should have 4 parts 
     // 1. state
@@ -76,24 +38,118 @@ function createStore(reducer){
     }
 }
 
+const ADD_TODO = 'ADD_TODO'
+const REMOVE_TODO = 'REMOVE_TODO'
+const TOGGLE_TODO = 'TOGGLE_TODO'
+const ADD_GOAL = 'ADD_GOAL'
+const REMOVE_GOAL = 'REMOVE_GOAL'
+
+function addTodoAction (todo) {
+    return {
+      type: ADD_TODO,
+      todo,
+    }
+  }
+  
+  function removeTodoAction (id) {
+    return {
+      type: REMOVE_TODO,
+      id,
+    }
+  }
+  
+  function toggleTodoAction (id) {
+    return {
+      type: TOGGLE_TODO,
+      id,
+    }
+  }
+  
+  function addGoalAction (goal) {
+    return {
+      type: ADD_GOAL,
+      goal,
+    }
+  }
+  
+  function removeGoalAction (id) {
+    return {
+      type: REMOVE_GOAL,
+      id,
+    }
+  }
+// updating the state 
+function todos(state = [], action){
+    switch(action.type){
+        case ADD_TODO:
+            // the reason we didn't use push is, bcs this function should be a pure funtion. 
+            //Not modifying the existing state but returning a new one.
+            return state.concat([action.todo])
+        case REMOVE_TODO:
+            // it will loop over each item in the state array, and filter one out
+            return state.filter((todo) => todo.id !== action.id)
+        case TOGGLE_TODO:
+            // instead of hard coding each property we want to toggle, 
+            //object.assign which allows us to merge different objects together
+            return state.map((todo) = todo.id !== action.id ? todo : 
+            Object.assign({},todo,{complete: !todo.complete}))
+            // says create me brand new object, get and merge todo object, but change the complete property
+        default:
+            return state;
+            }  
+    }
+
+function goals(state = [], action){
+    switch(action.type){
+        case ADD_GOAL:
+            return state.concat([action.goal])
+        case REMOVE_GOAL:
+            return state.filter((goal) => goal.id !== action.id)
+        default: 
+            return state;
+    }
+}
+
+function app(state={},action){
+    return{
+        todos: todos(state.todos,action),
+        goals: goals(state.goals,action)
+    }
+}
+
+
 const store = createStore(app)
+
+
 
 store.subscribe(() => {
     console.log("THE NEW STATE IS", store.getState())
 })
 
-store.dispatch({
-    type: 'ADD_TODO',
-    todo: {
-      id: 0,
-      name: 'Walk the dog',
-      complete: false,
-    }
-  })
-  store.dispatch({
-    type: 'ADD_GOAL',
-    goal: {
-      id: 0,
-      name: 'Learn Redux'
-    }
-  })
+store.dispatch(addTodoAction({
+    id: 0,
+    name: 'Walk the dog',
+    complete: false,
+  }))
+  
+  store.dispatch(addTodoAction({
+    id: 1,
+    name: 'Wash the car',
+    complete: false,
+  }))
+  
+  store.dispatch(addTodoAction({
+    id: 2,
+    name: 'Go to the gym',
+    complete: true,
+  }))
+  
+  store.dispatch(addGoalAction({
+    id: 0,
+    name: 'Learn Redux'
+  }))
+  
+  store.dispatch(addGoalAction({
+    id: 1,
+    name: 'Lose 20 pounds'
+  }))
